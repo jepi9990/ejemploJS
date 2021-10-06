bdd54484c62ad30b8e3ebbb9480abb8f869cae67
jepi9990 <jepi9990@hotmail.com>
2021-10-05 20:24:08 -0500

    prueba

---
+++ src/models/valores.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+const Valor = new Schema({
+    temperatura: String,
+    humedad: String,
+    fecha: Date 
+});
+    module.exports = mongoose.model('valores', Valor); 
