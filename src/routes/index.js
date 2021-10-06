9ba7959e4166adc66256269b739a58c506af19dd
jepi9990 <jepi9990@hotmail.com>
2021-10-05 21:03:20 -0500

    prueba

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const model = require('../models/valores')();
 
-router.get('/',(req,res)=>{
-    res.render('index.ejs',{
-        nombre:'Sergio'
-    })
-});
+router.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
+module.exports = router; 
 
 router.get('/saludo',(req,res)=>{
     res.send('Hola, este es el saludo desde express')
