421f0f12ef177889693060f0e9658435d4befa2d
jepi9990 <jepi9990@hotmail.com>
2021-12-20 11:49:20 -0600

    crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const { model } = require('mongoose');
+const { findOne } = require('../models/valores');
 const router = express.Router();
 const models = require('../models/valores')();
 
@@ -16,6 +20,28 @@ router.post('/add', async (req, res) => {
     const valor = new Valor(req.body);
     await valor.save();
     res.redirect('/');
+    res.json(valor);
 });
 
-module.exports = router; 
+router.get('/del/:id', async (req, res) => {
+    console.log(req.params.id);
+    const reg = await Valor.findByIdAndRemove(req.params.id);
+    console.log(reg);
+    res.redirect('/');
+    //res.status(200).json(reg);
+});
+
+router.get('/up/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const updates = req.body;
+        const options = { new: true };
+
+        const reg = await Valor.findByIdAndUpdate(id, updates, options);
+        res.send(reg);
+    } catch (error){
+        console.log(error.message);
+    }
+});
+
+module.exports = router;
