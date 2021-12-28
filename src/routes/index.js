96f1c2367ce623499d4b3ff3c1fcb83521121a3d
jepi9990 <jepi9990@hotmail.com>
2021-12-27 19:14:14 -0600

    js_2.0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const { model } = require('mongoose');
+const { findOne } = require('../models/valores');
 const router = express.Router();
 const models = require('../models/valores')();
 
@@ -18,4 +22,30 @@ router.post('/add', async (req, res) => {
     res.redirect('/');
 });
 
-module.exports = router; 
+router.get('/del/:id', async (req, res) => {
+    console.log(req.params.id);
+    const reg = await Valor.findByIdAndRemove(req.params.id);
+    console.log(reg);
+    res.redirect('/');
+    //res.status(200).json(reg);
+});
+
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const valor = await Valor.findById(req.params.id).lean()
+        res.render('update.ejs', { valor });
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+router.post('/edit/:id', async (req, res) => {
+
+    const { id } = req.params;
+    await Valor.findByIdAndUpdate(id, req.body);
+    res.redirect('/');
+
+});
+
+
+module.exports = router;
